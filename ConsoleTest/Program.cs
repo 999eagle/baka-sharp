bef18ecbf29a152db4912940af79793773edd9c6
Erik Tauchert <erik@tauchert.de>
2017-04-08 20:38:23 +0200

    add configuration

--- ConsoleTest/Program.cs
+++ ConsoleTest/Program.cs
@@ -4,25 +4,32 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
 namespace ConsoleTest
 {
 	class Program
 	{
+		private static IConfiguration config;
 		static void Main(string[] args)
 		{
+			var configBuilder = new ConfigurationBuilder();
+			configBuilder.AddJsonFile("config.json");
+			config = configBuilder.Build();
 			AsyncMain(args).Wait();
 			Console.ReadLine();
 		}
 
 		static async Task AsyncMain(string[] args)
 		{
-			var config = new BakaCore.Config();
-			var bot = new BakaCore.BakaChan(config);
-			config.LoggerFactory.AddConsole(LogLevel.Debug);
-			//Console.CancelKeyPress += (sender, e) => { bot.Stop(); };
-			var runTask = bot.Run("<token>");
+			var generalConfig = config.GetSection("General");
+
+			var bakaConfig = new BakaCore.Config();
+			bakaConfig.CommandTag = generalConfig["CommandTag"];
+			var bot = new BakaCore.BakaChan(bakaConfig);
+			bakaConfig.LoggerFactory.AddConsole(generalConfig.GetValue<LogLevel>("LogLevel"));
+			var runTask = bot.Run(config["Discord:LoginToken"]);
 			while (Console.ReadLine() != "e") ;
 			bot.Stop();
 			await runTask;
