0f8728d4effe4b1c5ace6ba98b14e6b86555d662
Erik Tauchert <erik@tauchert.de>
2017-04-09 18:06:50 +0200

    add give coins command

--- BakaCore/Commands/CoinsCommands.cs
+++ BakaCore/Commands/CoinsCommands.cs
@@ -31,7 +31,8 @@ namespace BakaCore.Commands
 			return new(MethodInfo, ICommandDescription)[] {
 				(typeInfo.GetMethod(nameof(GetCoinsCommand)), new CommandDescription(config.Currency.CurrencyCommand) { Help = $"Shows how many {config.Currency.CurrencyName} you or another user has." }),
 				(typeInfo.GetMethod(nameof(SpawnCoinsCommand)), new CommandDescription(config.Currency.CurrencyCommand) { Subcommand = "spawn", Help = $"Spawns {config.Currency.CurrencyName} on a user." }),
-				(typeInfo.GetMethod(nameof(DespawnCoinsCommand)), new CommandDescription(config.Currency.CurrencyCommand) { Subcommand = "despawn", Help = $"Depawns {config.Currency.CurrencyName} from a user." })
+				(typeInfo.GetMethod(nameof(DespawnCoinsCommand)), new CommandDescription(config.Currency.CurrencyCommand) { Subcommand = "despawn", Help = $"Despawns {config.Currency.CurrencyName} from a user." }),
+				(typeInfo.GetMethod(nameof(GiveCoinsCommand)), new CommandDescription(config.Currency.CurrencyCommand) { Subcommand = "give", Help = $"Give {config.Currency.CurrencyName} to someone else." })
 			};
 		}
 		
@@ -42,7 +43,7 @@ namespace BakaCore.Commands
 				var data = dataStore.GetGuildData(channel.Guild);
 				if (user == null)
 					user = message.Author;
-				await channel.SendMessageAsync($"{user.Mention} has {data.GetCoins(user)} {config.Currency.CurrencyName}");
+				await channel.SendMessageAsync($"{user.Mention} has {data.GetCoins(user)} {config.Currency.CurrencyName}.");
 			}
 		}
 
@@ -59,7 +60,7 @@ namespace BakaCore.Commands
 				int coins = data.GetCoins(user) + amount;
 				data.SetCoins(user, coins);
 				logger.LogTrace($"[Spawn] Set coins of user {user.Id} in guild {channel.Guild.Id} to {coins}.");
-				await channel.SendMessageAsync($"{user.Mention} now has {coins} {config.Currency.CurrencyName}");
+				await channel.SendMessageAsync($"{user.Mention} now has {coins} {config.Currency.CurrencyName}.");
 			}
 		}
 
@@ -82,7 +83,39 @@ namespace BakaCore.Commands
 				coins -= amount;
 				data.SetCoins(user, coins);
 				logger.LogTrace($"[Despawn] Set coins of user {user.Id} in guild {channel.Guild.Id} to {coins}.");
-				await channel.SendMessageAsync($"{user.Mention} now has {coins} {config.Currency.CurrencyName}");
+				await channel.SendMessageAsync($"{user.Mention} now has {coins} {config.Currency.CurrencyName}.");
+			}
+		}
+
+		public async Task GiveCoinsCommand(SocketMessage message, SocketUser user, int amount)
+		{
+			if (message.Channel is SocketTextChannel channel)
+			{
+				if (message.Author.Id == user.Id)
+				{
+					await channel.SendMessageAsync($"No need to give your own {config.Currency.CurrencyName} to yourself.");
+					return;
+				}
+				if (amount <= 0)
+				{
+					await channel.SendMessageAsync($"{message.Author.Mention} wanted to steal {config.Currency.CurrencyName} from {user.Mention}!");
+					return;
+				}
+				var data = dataStore.GetGuildData(channel.Guild);
+				int coinsReceiver = data.GetCoins(user);
+				int coinsSender = data.GetCoins(message.Author);
+				if (coinsSender < amount)
+				{
+					await channel.SendMessageAsync($"{message.Author.Mention} has only {coinsSender} {config.Currency.CurrencyName}.");
+					return;
+				}
+				coinsSender -= amount;
+				coinsReceiver += amount;
+				data.SetCoins(user, coinsReceiver);
+				logger.LogTrace($"[Give] Set coins of user {user.Id} in guild {channel.Guild.Id} to {coinsReceiver}.");
+				data.SetCoins(message.Author, coinsSender);
+				logger.LogTrace($"[Give] Set coins of user {message.Author.Id} in guild {channel.Guild.Id} to {coinsSender}.");
+				await channel.SendMessageAsync($"{user.Mention} received {amount} {config.Currency.CurrencyName} from {message.Author.Mention} and now has {coinsReceiver} {config.Currency.CurrencyName}.");
 			}
 		}
 	}
