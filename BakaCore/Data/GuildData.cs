0727eb1f76fb058c367c8092f71831aa47061b77
Erik Tauchert <erik@tauchert.de>
2017-04-16 19:28:54 +0200

    add storing server settings

--- BakaCore/Data/GuildData.cs
+++ BakaCore/Data/GuildData.cs
@@ -10,16 +10,29 @@ namespace BakaCore.Data
 	class GuildData
 	{
 		private IDictionary<ulong, int> coins;
+		private IDictionary<string, string> settings;
 		private Configuration config;
 
 		public ulong GuildId { get; }
 		public bool IsDirty { get; set; }
 		public event Action<GuildData, ulong> CoinsChanged;
+		public event Action<GuildData> SettingsChanged;
 
-		public GuildData(Configuration config, ulong guildId)
+		public GuildData(Configuration config, ulong guildId, bool setDefaults)
 		{
 			this.config = config;
 			GuildId = guildId;
+			if (setDefaults)
+			{
+				coins = new Dictionary<ulong, int>();
+				settings = new Dictionary<string, string>()
+				{
+					{ "welcomeChannel", "" },
+					{ "strike_3", "kick" },
+					{ "strike_4", "mutetext,mutevoice" },
+					{ "strike_5", "ban" }
+				};
+			}
 		}
 
 		public IDictionary<ulong, int> GetCoinData()
@@ -27,9 +40,16 @@ namespace BakaCore.Data
 			return coins;
 		}
 
+		public IDictionary<string, string> GetSettingsData() => settings;
+
 		public void SetCoinData(IDictionary<ulong, int> coins)
 		{
 			this.coins = coins;
+		}
+
+		public void SetSettingsData(IDictionary<string, string> settings)
+		{
+			this.settings = settings;
 		}
 
 		public int GetCoins(SocketUser user)
@@ -47,6 +67,32 @@ namespace BakaCore.Data
 				this.coins.Add(user.Id, coins);
 			IsDirty = true;
 			CoinsChanged?.Invoke(this, user.Id);
+		}
+
+		public string GetStrikeAction(int strikes)
+		{
+			if (settings.TryGetValue($"strike_{strikes}", out var action))
+				return action;
+			else
+				return null;
+		}
+		public void SetStrikeAction(int strikes, string action)
+		{
+			if (strikes > 7) return;
+			settings[$"strike_{strikes}"] = action;
+			SettingsChanged?.Invoke(this);
+		}
+		public ulong GetWelcomeChannel()
+		{
+			if (settings.TryGetValue("welcomeChannel", out var channel) && UInt64.TryParse(channel, out var channelId))
+				return channelId;
+			else
+				return 0;
+		}
+		public void SetWelcomeChannel(ulong channel)
+		{
+			settings["welcomeChannel"] = channel.ToString();
+			SettingsChanged?.Invoke(this);
 		}
 	}
 }
--- BakaCore/Data/JsonStore.cs
+++ BakaCore/Data/JsonStore.cs
@@ -20,6 +20,7 @@ namespace BakaCore.Data
 		private ILogger logger;
 
 		private Task saveCoinsTask = null;
+		private Task saveSettingsTask = null;
 		private CancellationTokenSource scheduledTaskCancelWait = new CancellationTokenSource();
 
 		public JsonStore(Configuration config, ILoggerFactory loggerFactory)
@@ -35,12 +36,17 @@ namespace BakaCore.Data
 			{
 				saveCoinsTask.Wait();
 			}
+			if (saveSettingsTask != null)
+			{
+				saveSettingsTask.Wait();
+			}
 		}
 
-		private GuildData CreateNewGuildData(ulong guildId)
+		private GuildData CreateNewGuildData(ulong guildId, bool setDefaults)
 		{
-			var data = new GuildData(config, guildId);
+			var data = new GuildData(config, guildId, setDefaults);
 			data.CoinsChanged += GuildDataCoinsChanged;
+			data.SettingsChanged += GuildDataSettingsChanged;
 			return data;
 		}
 
@@ -90,6 +96,53 @@ namespace BakaCore.Data
 				logger.LogInformation("Saved coins data.");
 			}
 		}
+		private void GuildDataSettingsChanged(GuildData guild)
+		{
+			ScheduleSaveSettings();
+		}
+
+		private void ScheduleSaveSettings()
+		{
+			if (saveSettingsTask != null && saveSettingsTask.IsCompleted)
+			{
+				saveSettingsTask.GetAwaiter().GetResult();
+				saveSettingsTask = null;
+			}
+
+			if (saveSettingsTask == null)
+			{
+				saveSettingsTask = SaveSettingsTask();
+				logger.LogInformation("Scheduled saving settings.");
+			}
+
+			async Task SaveSettingsTask()
+			{
+				try
+				{
+					await Task.Delay(TimeSpan.FromSeconds(30), scheduledTaskCancelWait.Token);
+				}
+				catch (TaskCanceledException) { }
+				using (var file = File.Open(".\\data\\settings.json", FileMode.Create, FileAccess.Write))
+				using (var writer = new StreamWriter(file))
+				{
+					var jObj = new JObject();
+					foreach (var kv in guildData)
+					{
+						var guildObj = new JObject();
+						foreach (var kv2 in kv.Value.GetSettingsData())
+						{
+							guildObj.Add(kv2.Key, kv2.Value);
+						}
+						jObj.Add(kv.Key.ToString(), guildObj);
+						kv.Value.IsDirty = false;
+					}
+					await writer.WriteAsync(await Task.Factory.StartNew(() => JsonConvert.SerializeObject(jObj)));
+					await writer.FlushAsync();
+				}
+				logger.LogInformation("Saved settings data.");
+			}
+		}
+
 
 		private void LoadData()
 		{
@@ -105,9 +158,25 @@ namespace BakaCore.Data
 					{
 						var guildId = UInt64.Parse(property.Name);
 						if (!data.ContainsKey(guildId))
-							data.Add(guildId, CreateNewGuildData(guildId));
+							data.Add(guildId, CreateNewGuildData(guildId, true));
 						var guild = data[guildId];
 						guild.SetCoinData(((JObject)property.Value).Properties().ToDictionary(p => UInt64.Parse(p.Name), p => (int)p.Value));
+					}
+				}
+			}
+			if (File.Exists(".\\data\\settings.json"))
+			{
+				using (var file = File.OpenText(".\\data\\settings.json"))
+				using (var reader = new JsonTextReader(file))
+				{
+					var settings = (JObject)JToken.ReadFrom(reader);
+					foreach (var property in settings.Properties())
+					{
+						var guildId = UInt64.Parse(property.Name);
+						if (!data.ContainsKey(guildId))
+							data.Add(guildId, CreateNewGuildData(guildId, true));
+						var guild = data[guildId];
+						guild.SetSettingsData(((JObject)property.Value).Properties().ToDictionary(p => p.Name, p => (string)p.Value));
 					}
 				}
 			}
@@ -119,7 +188,7 @@ namespace BakaCore.Data
 			if (guildData == null)
 				LoadData();
 			if (!guildData.ContainsKey(guild.Id))
-				guildData.Add(guild.Id, CreateNewGuildData(guild.Id));
+				guildData.Add(guild.Id, CreateNewGuildData(guild.Id, true));
 			return guildData[guild.Id];
 		}
 	}
