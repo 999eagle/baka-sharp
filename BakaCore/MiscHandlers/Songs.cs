2cb413009da6a58b920cd0ad935e0d765bc2991e
Erik Tauchert <999eagle@999eagle.moe>
2017-09-01 18:22:59 +0200

    Refactor Songs and Greeting into misc handlers

--- BakaCore/MiscHandlers/Songs.cs
+++ BakaCore/MiscHandlers/Songs.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,7 +8,7 @@ using Microsoft.Extensions.Logging;
 
 using Discord.WebSocket;
 
-namespace BakaCore
+namespace BakaCore.MiscHandlers
 {
 	class Songs : IDisposable
 	{
