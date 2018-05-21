6f66297e75756f7cc9dbd37da0498d0f7a7ce8b3
Sophie Tauchert <999eagle@999eagle.moe>
2018-05-21 20:51:49 +0200

    Use system proxy for discord connections

--- BakaCore/BakaChan.cs
+++ BakaCore/BakaChan.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -57,7 +58,9 @@ namespace BakaCore
 				services
 					.AddScoped((_) => new DiscordSocketConfig()
 					{
-						LogLevel = LogSeverity.Debug
+						LogLevel = LogSeverity.Debug,
+						RestClientProvider = Discord.Net.Rest.DefaultRestClientProvider.Create(useProxy: true),
+						WebSocketProvider = Discord.Net.WebSockets.DefaultWebSocketProvider.Create(WebRequest.DefaultWebProxy)
 					})
 					.AddScoped<DiscordSocketClient>()
 					.AddScoped<Commands.CommandHandler>()
