32878e2e8964cae2c4942201540553303a34e43c
Sophie Tauchert <999eagle@999eagle.moe>
2018-07-15 18:06:35 +0200

    Handle C-c and signals for exiting

--- BakaChan/Program.cs
+++ BakaChan/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 using Microsoft.Extensions.Configuration;
@@ -23,12 +24,48 @@ namespace ConsoleTest
 		static async Task AsyncMain(string[] args)
 		{
 			var bakaConfig = config.Get<BakaCore.Configuration>();
-			var bot = new BakaCore.BakaChan(bakaConfig);
+			bakaConfig.Logging.LoggerFactory = new LoggerFactory();
 			bakaConfig.Logging.LoggerFactory.AddConsole(bakaConfig.Logging.LogLevel);
-			var runTask = bot.Run();
-			while (Console.ReadLine() != "e") ;
-			bot.Stop();
-			await runTask;
+			var logger = bakaConfig.Logging.LoggerFactory.CreateLogger("Baka-chan");
+			logger.LogInformation("Initializing Baka-chan");
+			var bot = new BakaCore.BakaChan(bakaConfig);
+			Task runTask = null;
+
+			Console.CancelKeyPress += (sender, evArgs) => { Exit(); };
+			AppDomain.CurrentDomain.ProcessExit += (sender, evArgs) => { Exit(); };
+
+			logger.LogDebug("Starting bot instance");
+			try
+			{
+				runTask = bot.Run();
+			}
+			catch (Exception ex)
+			{
+				logger.LogCritical(ex, "Exception thrown during bot startup");
+				Exit();
+			}
+			if (runTask != null)
+			{
+				await runTask;
+			}
+
+			void Exit()
+			{
+				logger.LogDebug("Stopping bot instance");
+				bot.Stop();
+				if (runTask != null)
+				{
+					try
+					{
+						runTask.Wait();
+					}
+					catch (Exception ex)
+					{
+						logger.LogCritical(ex, "Exception thrown during bot shutdown");
+					}
+				}
+				logger.LogInformation("Bot instance stopped");
+			}
 		}
 	}
 }
