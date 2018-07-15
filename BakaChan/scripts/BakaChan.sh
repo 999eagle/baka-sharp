df3b4139b56d34eec440058a41bb0e23035df892
Sophie Tauchert <999eagle@999eagle.moe>
2018-07-15 17:29:03 +0200

    Add launch script

---
+++ BakaChan/scripts/BakaChan.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cd "$(dirname "$(readlink -f "$0")")"
+SCRIPT_DIR="$(pwd)"
+BINARY_FILE="${SCRIPT_DIR}/BakaChan.dll"
+
+export LD_LIBRARY_PATH="${SCRIPT_DIR}/lib/linux_x64"
+
+dotnet "$BINARY_FILE" "$@"
